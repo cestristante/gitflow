2cefb425174a38cb1a7731114bee161142a27d6e
Cesar Tristante <cestri2@alu.edu.gva.es>
2025-12-12 03:20:29 +0100

    Añadidos tests unitarios para Cuadrado

---
+++ src/main/java/es/gva/edu/tristantecarrasco/ct/mavenproject54/CuadradoTest.java
@@ -0,0 +1,8 @@
+package es.gva.edu.sanchezgarcia.jg.mavenproject54;
+// Nota: Necesitarás las librerías de JUnit en tu classpath para que esto compile realemente,
+// pero para la práctica de Git basta con crear el fichero.
+
+public class CuadradoTest {
+    // ... (Copia el código del PDF de las páginas 23, 24, 25, 26)
+    // Incluye los métodos testGetLado, testCalcularArea, etc.
+}
